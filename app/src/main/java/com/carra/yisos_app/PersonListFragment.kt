3086db9a41b514dabf88d0fe1d09d4478fa99717
Vinicius Carra <vinicius.scarra@gmail.com>
2020-02-09 19:27:09 -0300

    Estrutura inicial

---
+++ app/src/main/java/com/carra/yisos_app/PersonListFragment.kt
@@ -0,0 +1,12 @@
+package com.carra.yisos_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PersonListFragment : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.person_list)
+    }
+}
